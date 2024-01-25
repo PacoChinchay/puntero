849f921925d362d70e7b052f8a1d7bcc6670fec1
PacoChinchay <pacochinchay350@gmail.com>
2024-01-24 23:04:34 -0500

    puntero

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+     const handleMove = (event) => {
+       const { clientX, clientY } = event;
+       setPosition({ x: clientX, y: clientY });
+     };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+  );
+};
+
+function App() {
+
+  return (
+    <main>
+      <FollowMouse/> 
+    </main>
+  )
+}
+export default App;
